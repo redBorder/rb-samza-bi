ee93a18ceee99571036101817b4068b08650d7d4
Carlos Rodriguez <crodriguez@redborder.net>
2015-03-24 19:15:17 +0100

    The task now reads from rb_flow and writes an exact copy of each message on rb_flow_with_nmsp

--- src/main/java/net/redborder/samza/tasks/NmspEnrichStreamTask.java
+++ src/main/java/net/redborder/samza/tasks/NmspEnrichStreamTask.java
@@ -16,37 +16,40 @@
 package net.redborder.samza.tasks;
 
 import org.apache.samza.config.Config;
-import org.apache.samza.storage.kv.KeyValueStore;
 import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.system.OutgoingMessageEnvelope;
 import org.apache.samza.system.SystemStream;
 import org.apache.samza.task.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class NmspEnrichStreamTask implements StreamTask, InitableTask {
-    private KeyValueStore<String, Map<String, Object>> store;
+    private static final Logger log = LoggerFactory.getLogger(NmspEnrichStreamTask.class);
+
+    //private KeyValueStore<String, Map<String, Object>> store;
     private final SystemStream OUTPUT_STREAM = new SystemStream("kafka", "rb_flow_with_nmsp");
 
     @Override
     public void init(Config config, TaskContext context) throws Exception {
-        this.store = (KeyValueStore<String, Map<String, Object>>) context.getStore("nmsp");
+        //this.store = (KeyValueStore<String, Map<String, Object>>) context.getStore("nmsp");
     }
 
     @Override
     public void process(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator) throws Exception {
         Map<String, Object> message = (Map<String, Object>) envelope.getMessage();
         String mac = (String) message.get("client_mac");
+        log.info("Client mac is " + mac);
 
-        if (envelope.getSystemStreamPartition().getSystemStream().getStream().equals("rb_nmsp")) {
-            this.store.put(mac, message);
-        } else {
-            Map<String, Object> output = new HashMap<>();
-            output.putAll(message);
-            output.putAll(this.store.get(mac));
+        //if (envelope.getSystemStreamPartition().getSystemStream().getStream().equals("rb_nmsp")) {
+           // this.store.put(mac, message);
+        //} else {
+            //Map<String, Object> output = new HashMap<>();
+            //output.putAll(message);
+            //output.putAll(this.store.get(mac));
 
-            collector.send(new OutgoingMessageEnvelope(OUTPUT_STREAM, mac, output));
-        }
+            collector.send(new OutgoingMessageEnvelope(OUTPUT_STREAM, null, message));
+        //}
     }
 }
