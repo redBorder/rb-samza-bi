dd55c47a2ec13d941c3aade4a219457e1d8ecb7a
Carlos Rodriguez <crodriguez@redborder.net>
2015-04-08 09:20:59 +0200

    Update to samza 0.9.0.
    Removed samza-serializers dependency since they are now stored on samza-api.
    Added license headers to mock classes.

--- src/test/java/net/redborder/samza/util/MockMetricsRegistry.java
+++ src/test/java/net/redborder/samza/util/MockMetricsRegistry.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015 ENEO Tecnologia S.L.
+ * This file is part of redBorder.
+ * redBorder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * redBorder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with redBorder. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.redborder.samza.util;
 
 import org.apache.samza.metrics.Counter;
@@ -5,9 +20,6 @@ import org.apache.samza.metrics.Gauge;
 import org.apache.samza.metrics.MetricsRegistry;
 import org.apache.samza.metrics.Timer;
 
-/**
- * Date: 7/4/15 13:45.
- */
 public class MockMetricsRegistry implements MetricsRegistry {
     @Override
     public Counter newCounter(String s, String s1) {
