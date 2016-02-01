4b23efcddb7e72350f92b50f28109bc3a854aa2f
Andrés Gomez <agomez@redborder.net>
2016-02-01 18:40:42 +0100

    Added sensor unallocate but with latlong.

--- src/main/java/net/redborder/samza/util/PostgresqlManager.java
+++ src/main/java/net/redborder/samza/util/PostgresqlManager.java
@@ -195,34 +195,24 @@ public class PostgresqlManager {
         try {
             if (conn != null) {
                 st = conn.createStatement();
-                rs = st.executeQuery("SELECT DISTINCT ON (access_points.mac_address) access_points.ip_address," +
-                        "  access_points.mac_address, access_points.enrichment, zones.name AS zone, access_points.latitude AS latitude," +
-                        "  access_points.longitude AS longitude, floors.name AS floor, floors.uuid AS floor_uuid," +
-                        "  buildings.name AS building, buildings.uuid AS building_uuid, campuses.name AS campus," +
-                        "  campuses.uuid AS campus_uuid, deployments.name AS deployment, deployments.uuid AS" +
-                        "  deployment_uuid, namespaces.name AS namespace, namespaces.uuid AS namespace_uuid, " +
-                        "  markets.name AS market, markets.uuid AS market_uuid, organizations.name AS organization," +
-                        "  organizations.uuid AS organization_uuid, service_providers.name AS service_provider," +
-                        "  service_providers.uuid AS service_provider_uuid FROM access_points" +
-                        "  JOIN sensors ON access_points.sensor_id = sensors.id" +
-                        "  LEFT JOIN access_points_zones AS zones_ids ON access_points.id = zones_ids.access_point_id" +
-                        "  LEFT JOIN zones ON zones_ids.zone_id = zones.id" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=101) AS floors" +
-                        "    ON floors.lft <= sensors.lft AND floors.rgt >= sensors.rgt" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=5) AS buildings" +
-                        "    ON buildings.lft <= sensors.lft AND buildings.rgt >= sensors.rgt" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=4) AS campuses" +
-                        "    ON campuses.lft <= sensors.lft AND campuses.rgt >= sensors.rgt" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=7) AS deployments" +
-                        "    ON deployments.lft <= sensors.lft AND deployments.rgt >= sensors.rgt" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=8) AS namespaces" +
-                        "    ON namespaces.lft <= sensors.lft AND namespaces.rgt >= sensors.rgt" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=3) AS markets" +
-                        "    ON markets.lft <= sensors.lft AND markets.rgt >= sensors.rgt" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=2) AS organizations" +
-                        "    ON organizations.lft <= sensors.lft AND organizations.rgt >= sensors.rgt" +
-                        "  LEFT JOIN (SELECT * FROM sensors WHERE domain_type=6) AS service_providers" +
-                        "    ON service_providers.lft <= sensors.lft AND service_providers.rgt >= sensors.rgt");
+                rs = st.executeQuery("SELECT DISTINCT ON (access_points.mac_address) access_points.ip_address, access_points.mac_address, access_points.enrichment," +
+                        " zones.name AS zone, access_points.latitude AS latitude, access_points.longitude AS longitude, floors.name AS floor, " +
+                        " floors.uuid AS floor_uuid, buildings.name AS building, buildings.uuid AS building_uuid, campuses.name AS campus, campuses.uuid AS campus_uuid," +
+                        " deployments.name AS deployment, deployments.uuid AS deployment_uuid, namespaces.name AS namespace, namespaces.uuid AS namespace_uuid," +
+                        " markets.name AS market, markets.uuid AS market_uuid, organizations.name AS organization, organizations.uuid AS organization_uuid," +
+                        " service_providers.name AS service_provider, service_providers.uuid AS service_provider_uuid" +
+                        " FROM access_points JOIN sensors ON (access_points.sensor_id = sensors.id OR access_points.switch_id = sensors.id)" +
+                        " LEFT JOIN access_points_zones AS zones_ids ON access_points.id = zones_ids.access_point_id" +
+                        " LEFT JOIN zones ON zones_ids.zone_id = zones.id" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=101) AS floors ON floors.lft <= sensors.lft AND floors.rgt >= sensors.rgt" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=5) AS buildings ON buildings.lft <= sensors.lft AND buildings.rgt >= sensors.rgt" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=4) AS campuses ON campuses.lft <= sensors.lft AND campuses.rgt >= sensors.rgt" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=7) AS deployments ON deployments.lft <= sensors.lft AND deployments.rgt >= sensors.rgt" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=8) AS namespaces ON namespaces.lft <= sensors.lft AND namespaces.rgt >= sensors.rgt" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=3) AS markets ON markets.lft <= sensors.lft AND markets.rgt >= sensors.rgt" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=2) AS organizations ON organizations.lft <= sensors.lft AND organizations.rgt >= sensors.rgt" +
+                        " LEFT JOIN (SELECT * FROM sensors WHERE domain_type=6) AS service_providers ON service_providers.lft <= sensors.lft AND service_providers.rgt >= sensors.rgt");
+
 
                 ObjectMapper mapper = new ObjectMapper();
 
